75153999a496cea3607cde13a379b5ae041c965d
charlie <cwiechec@buffalo.edu>
2019-12-14 19:20:32 -0500

    testing key

--- chap12/test8.sh
+++ chap12/test8.sh
@@ -1,1 +1,10 @@
  #!/usr/bin/env bash
+# testing string equality
+testuser=baduser
+#
+if [ $USER != $testuser ]
+then
+    echo "[-] $USER is not $testuser!"
+else
+    echo "[+] Welcome $testuser!"
+fi
